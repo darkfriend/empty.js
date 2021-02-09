f254eb785d195649cdece259b2d975c5cef018c8
darkfriend <maslov.tlt@gmail.com>
2021-02-10 01:49:48 +0300

    remove comments

--- src/emptyMode.js
+++ src/emptyMode.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/**
- * Check variable on empty
- * @author darkfriend <hi@darkfriend.ru>
- * @param {*} variable
- * @param {int} mode Bitwise for any mode (0 - no strict, 1 - then check result function, 2 - then 0 not empty, 4 - then "" not empty, 8 - then "{}" not empty )
- * @return {boolean}
- */
-
 const emptyMode = {
     base: 0, // base mode
     function: 1, // check result function
@@ -18,5 +10,4 @@ const emptyMode = {
 
 module.exports = emptyMode;
 
-// Allow use of default import syntax in TypeScript
 module.exports.default = emptyMode;
